084f3f5b802e0cb9ef5537d507c507c46b70371d
Noel Miller <noeldamonmiller@gmail.com>
2023-04-12 09:59:36 -0700

    user_preferences needed these changes to update a single field and not set everything to null

--- src/main/java/co/inajar/oursponsors/services/user/UserManagerImpl.java
+++ src/main/java/co/inajar/oursponsors/services/user/UserManagerImpl.java
@@ -9,7 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @Transactional
@@ -37,17 +40,16 @@ public class UserManagerImpl implements UserManager {
     @Override
     public Preferences updateUserPreferences(PreferencesRequest request) {
         var preferences = getPreferencesByUserId(request.getUserId());
-        preferences.setMyStateOnly(request.getMyStateOnly());
-        preferences.setMyPartyOnly(request.getMyPartyOnly());
-        preferences.setTwitterHide(request.getTwitterHide());
-        preferences.setFacebookHide(request.getFacebookHide());
-        preferences.setYoutubeHide(request.getYoutubehide());
-        preferences.setGoogleEntityHide(request.getGoogleEntityHide());
-        preferences.setCspanHide(request.getCspanHide());
-        preferences.setVoteSmartHide(request.getVoteSmartHide());
-        preferences.setGovTrackHide(request.getGovTrackHide());
-        preferences.setOpenSecretsHide(request.getOpenSecretsHide());
-        preferencesRepo.save(preferences);
+
+        if (request.getMyStateOnly() != null) preferences.setMyStateOnly(request.getMyStateOnly());
+        if (request.getMyPartyOnly() != null) preferences.setMyPartyOnly(request.getMyPartyOnly());
+        if (request.getTwitterHide() != null) preferences.setTwitterHide(request.getTwitterHide());
+        if (request.getFacebookHide() != null) preferences.setFacebookHide(request.getFacebookHide());
+        if (request.getYoutubehide() != null) preferences.setYoutubeHide(request.getYoutubehide());
+        if (request.getGoogleEntityHide() != null) preferences.setGoogleEntityHide(request.getGoogleEntityHide());
+        if (request.getCspanHide() != null) preferences.setCspanHide(request.getCspanHide());
+        if (request.getGovTrackHide() !=null) preferences.setGovTrackHide(request.getGovTrackHide());
+        if (request.getOpenSecretsHide() != null) preferences.setOpenSecretsHide(request.getOpenSecretsHide());
         return preferences;
     }
 }
