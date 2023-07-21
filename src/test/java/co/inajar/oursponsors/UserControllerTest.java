73267ccb03b5321b97ebde4750ee26a665728e4c
Noel Miller <noeldamonmiller@gmail.com>
2023-07-21 14:33:31 -0700

    unit_tests passing user controller test

--- src/test/java/co/inajar/oursponsors/UserControllerTest.java
+++ src/test/java/co/inajar/oursponsors/UserControllerTest.java
@@ -1,57 +1,62 @@
 import co.inajar.oursponsors.controllers.user.UserController;
 import co.inajar.oursponsors.dbos.entities.User;
-import co.inajar.oursponsors.models.user.UserResponse;
 import co.inajar.oursponsors.services.user.UserManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import java.util.Collections;
-import java.util.Map;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
-class UserControllerTest {
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
 
-    @Mock
+    private static final String GOOGLE_UID = "google-uid";
+
+    @Autowired
+    private WebApplicationContext wac;
+    private MockMvc mockMvc;
+
+    @MockBean
     private UserManager userManager;
 
-    @InjectMocks
-    private UserController userController;
+    @BeforeEach
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
 
     @Test
-    void testGetUser() {
+    public void testGetUsers() throws Exception {
 
-        // Mock the request headers
-        Map<String, String> headers = Collections.singletonMap("GOOGLE-UID", "someGoogleUid");
+        User user = new User();
+        when(userManager.getUserByGoogleUid(GOOGLE_UID)).thenReturn(Optional.of(user));
+        mockMvc.perform(get("/user/get_user"))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
 
-        // Mock the expected user response
-        User expectedUser = new User(); // Create a user instance with the desired data
-        expectedUser.setName("John Smith");
-        expectedUser.setEmail("john@gmail.com");
-        expectedUser.setGoogleUid("someGoogleUid");
-        UserResponse expectedResponse = new UserResponse(expectedUser);
-
-        // Mock the userManager.getUserByGoogleUid() method to return the expected user
-        when(userManager.getUserByGoogleUid(headers.get("GOOGLE-UID"))).thenReturn(Optional.of(expectedUser));
-
-        // Invoke the getUser() method
-        ResponseEntity<UserResponse> responseEntity = userController.getUser(headers);
-
-        // Verify the response status code
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-
-        // Verify the response body
-        UserResponse responseBody = responseEntity.getBody();
-        assertEquals(expectedResponse, responseBody);
-
+    @Configuration
+    @EnableAutoConfiguration
+    public static class TestConfig {
+        @Bean
+        public UserController userController() {
+            return new UserController();
+        }
     }
 
 }
