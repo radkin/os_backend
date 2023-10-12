ee469ae7af4e0c413da52fcd2fff91e3764caa06
Noel Miller <noeldamonmiller@gmail.com>
2023-10-12 08:48:52 -0700

    docs_and_tests added a helper method for generating tokens and adding more repo tests

---
+++ src/main/java/co/inajar/oursponsors/helpers/TokenGenerator.java
@@ -0,0 +1,51 @@
+package co.inajar.oursponsors.helpers;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class TokenGenerator {
+
+    public static void main(String[] args) {
+        String shaToken = generateSHAToken();
+        System.out.println("Generated SHA Token: " + shaToken);
+    }
+
+    // Method to generate a SHA-256 token
+    public static String generateSHAToken() {
+        try {
+            String randomString = generateRandomString(32); // Generate a random string of the desired length
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = md.digest(randomString.getBytes());
+
+            // Convert the byte array to a hexadecimal string
+            StringBuilder hexString = new StringBuilder();
+            for (byte hashByte : hashBytes) {
+                String hex = Integer.toHexString(0xff & hashByte);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            // Handle the exception
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // Method to generate a random string of a specified length
+    public static String generateRandomString(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder randomString = new StringBuilder();
+        SecureRandom secureRandom = new SecureRandom();
+        for (int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(characters.length());
+            randomString.append(characters.charAt(randomIndex));
+        }
+        return randomString.toString();
+    }
+}
+
--- src/test/java/co/inajar/oursponsors/repos/UserRepoTests.java
+++ src/test/java/co/inajar/oursponsors/repos/UserRepoTests.java
@@ -2,10 +2,13 @@ package co.inajar.oursponsors.repos;
 
 import co.inajar.oursponsors.dbos.entities.User;
 import co.inajar.oursponsors.dbos.repos.UserRepo;
+import co.inajar.oursponsors.helpers.TokenGenerator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class UserRepoTests {
@@ -14,13 +17,15 @@ public class UserRepoTests {
     private UserRepo userRepo;
 
     @Test
-    public void UserRepo_SaveAll_ReturnSavedRestApiKey() {
+    public void UserRepo_SaveAll_ReturnSavedUser() {
+
+        String token = TokenGenerator.generateSHAToken();
 
         // arrange
         User user = User.builder()
                 .firstName("fred")
                 .lastName("flintstone")
-                .apiKey("yippie")
+                .apiKey(token)
                 .build();
 
 
@@ -32,7 +37,69 @@ public class UserRepoTests {
         // assert
 
         Assertions.assertThat(savedUser).isNotNull();
-        
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+    }
 
+    @Test
+    public void UserRepo_GetAll_ReturnMoreThanOneUser() {
+
+        String token1 = TokenGenerator.generateSHAToken();
+        String token2 = TokenGenerator.generateSHAToken();
+
+        User user = User.builder()
+                .firstName("fred")
+                .lastName("flintstone")
+                .apiKey(token1)
+                .build();
+
+        User user2 = User.builder()
+                .firstName("fred")
+                .lastName("flintstone")
+                .apiKey(token2)
+                .build();
+
+        userRepo.save(user);
+        userRepo.save(user2);
+
+        List<User> userList = userRepo.findAll();
+
+        Assertions.assertThat(userList).isNotNull();
+        Assertions.assertThat(userList.size()).isGreaterThan(0);
+    }
+
+    @Test
+    public void UserRepo_FindById_ReturnUser() {
+
+        String token = TokenGenerator.generateSHAToken();
+
+        User user = User.builder()
+                .firstName("fred")
+                .lastName("flintstone")
+                .apiKey(token)
+                .build();
+
+        userRepo.save(user);
+
+        User foundUser = userRepo.findById(user.getId()).get();
+
+        Assertions.assertThat(foundUser).isNotNull();
+    }
+
+    @Test
+    public void UserRepo_FindUserByApiKey_ReturnUser() {
+
+        String token = TokenGenerator.generateSHAToken();
+
+        User user = User.builder()
+                .firstName("fred")
+                .lastName("flintstone")
+                .apiKey(token)
+                .build();
+
+        userRepo.save(user);
+
+        User foundUser = userRepo.findUserByApiKey(user.getApiKey()).get();
+
+        Assertions.assertThat(foundUser.getApiKey()).isEqualTo(user.getApiKey());
     }
 }
